6488b78e7734b405c04c6eb6083fbe13000b7963
zhaojy <42832331+spSAKURA@users.noreply.github.com>
2021-08-01 12:14:19 +0800

    情报列表接口

---
+++ Application/Oa/Controller/ContainerController.class.php
@@ -0,0 +1,42 @@
+<?php
+namespace Oa\Controller;
+
+use Oa\Model\ContainerDetailModel;
+
+class ContainerController extends AuthController{
+    public function getlist (){
+        $condition = $_REQUEST['condition'];
+        $query = [];
+        //模糊查询字段
+        $likeCondition = [
+            'bkg_no' => 'bkg_no',
+            'booker_place' => 'booker_place',
+            'transprotation' => 'transprotation',
+            'booker' => 'booker',
+        ];
+        foreach($likeCondition as $conditionName =>$colNmae){
+            if($condition[$conditionName]){
+                $query[$colNmae] = [
+                    'LIKE',
+                    '%'.$condition[$conditionName].'%',
+                ];
+            }
+        }
+        $timeCondition = [
+            'cy_cut'=>'cy_cut',
+            'vanning_date'=>'vanning_date',
+        ];
+        foreach($timeCondition as $conditionName =>$colNmae){
+            if($condition[$conditionName][0] && $condition[$conditionName][1]){
+                $query[$colNmae] = [
+                    'BETWEEN',
+                    [$condition[$conditionName][0],
+                    $condition[$conditionName][1],]
+                ];
+            }
+        }
+        $current = $_REQUEST['page']?:0;
+        $size = $_REQUEST['page_size']?:100;
+        $this->ajaxSuccess((new ContainerDetailModel())->getList($query, $size, $current));
+    }
+}
--- Application/Oa/Model/ContainerDetailModel.class.php
+++ Application/Oa/Model/ContainerDetailModel.class.php
@@ -20,4 +20,47 @@ class ContainerDetailModel extends BkgCommonModel {
             'bkg_id' => $bkg_id,
         ])->select();
     }
+    
+    public function getList($query,$size=100,$current=0){
+        $info['total'] = $this
+            ->_beforeQuery($query)
+            ->count();
+        $info['list'] = $this
+            ->_beforeQuery($query)
+            ->field([
+                'c.id',              #id
+                'bkg_id',            #主表id
+                'vanning_date',      #バンニング日
+                'booker',            #booker
+                'booker_place',      #バンニング場所
+                'transprotation',    #運送業 
+                'cy_cut',            #CY CUT
+                'bkg_date',          #BKG DATE
+                'CONCAT(l.`port`,"/",d.`port`) as ld',
+                                     #POL/POD
+                'bkg_no',            #BKG NO
+            ])
+            ->limit($current * $size, $size)
+            ->order('vanning_date desc')
+            ->select();
+            // die($this->getLastSql());
+        return $info;
+    }
+    protected function _beforeQuery($query){
+        return $this
+            ->alias('c')
+            ->join('bkg AS b ON b.id = c.bkg_id')
+            ->join('port_of_loading AS l ON l.id = c.bkg_id')
+            ->join('port_of_delovery AS d ON d.id = c.bkg_id')
+            ->join('trader AS t ON t.id = c.bkg_id')
+            ->where([
+                'b.delete_at' => ['exp','IS NULL'],
+                'c.delete_at' => [
+                    ['exp','IS NULL'],
+                    '',
+                    'or',
+                ],
+            ])
+            ->where($query);
+    }
 }
