f15e538bc61d2b99a80958f1d03ca04752a212bf
zjy <zhaojangyu@gmail.com>
2021-08-17 18:08:36 +0800

    完成

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -27,6 +27,17 @@ function rmSepStr($mixstr){
         $replace = [
             '　' => ' ',
             'ー' => '-',
+            '１'=> '1',
+            '２'=> '2',
+            '３'=> '3',
+            '４'=> '4',
+            '５'=> '5',
+            '６'=> '6',
+            '７'=> '7',
+            '８'=> '8',
+            '９'=> '9',
+            '０'=> '0',
+            "\n" => '<br/>',
         ];
         return str_replace(array_keys($replace),array_values($replace),$mixstr);
     }
