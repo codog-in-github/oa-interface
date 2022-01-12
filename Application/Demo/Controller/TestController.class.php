2fc135a225296be41a7df6980e2a3e7c4077038c
zjy <zhaojangyu@gmail.com>
2022-01-12 21:30:08 +0800

    1

--- Application/Demo/Controller/TestController.class.php
+++ Application/Demo/Controller/TestController.class.php
@@ -15,11 +15,11 @@ class TestController extends Controller
         foreach($data as &$record){
             $match = [];
             // AI  THE ROAD(AIROA)
-            $preg = '/^[A-Z]+ (.+)\(([A-Z]+)\)$/';
+            $preg = '/^(.+)(\([A-Z]+) ([A-Z]+\))$/';
             preg_match_all($preg, $record['port'], $match);
             // print_r($match);
-            $record['port'] = $match[0] ? trim($match[1][0]).'('.$record['country'].' '.$match[2][0].')' : $record['port'];
-            M($tableName)->save($record);
+            echo $record['port'] = $match[0] ? $match[1][0].$match[2][0].$match[3][0] : $record['port'], "\n";
+            // M($tableName)->save($record);
         }
     }
 }
