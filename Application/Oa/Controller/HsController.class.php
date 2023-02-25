503a89650b3d0b899679edb0ce44ae888ddf7c5a
zjy <zhaojangyu@outlook.com>
2023-02-25 11:40:43 +0800

    1

--- Application/Oa/Controller/HsController.class.php
+++ Application/Oa/Controller/HsController.class.php
@@ -24,16 +24,49 @@ class HsController extends AuthController {
         $excel = PHPExcel_IOFactory::createReader($IOFactoryType)->load($file);
         if($_POST['config']) {
             $config = json_decode($_POST['config']);
+            $needUpdate = $config->update === 1;
+            $emptyOnly = $config->emptyOnly === 1;
             $map = [];
-            foreach( M('hs')->select() as $row) {
+            $idMap = [];
+            $updateData = [];
+            $addData = [];
+            foreach(M('hs')->select() as $row) {
                 $map[$row['description']] = $row['hs'];
+                $idMap[$row['description']] = $row['id'];
             }
             foreach($config->sheets as $index) {
-                $this->fillSheet($excel, $index, $map, $config->emptyOnly === 1);
+                $data = $this->fillSheet($excel, $index, $map, $emptyOnly);
+                if($needUpdate) {
+                    foreach ($data as $description => $hs) {
+                        if($idMap[$description]) {
+                            $updateData[$description] = $hs;
+                        }  else {
+                            $addData[$description] = $hs;
+                        }
+                    }
+                }
             }
-
+            if($needUpdate) {
+                $model = M('hs');
+                if($updateData) {
+                    $model->save([
+                        'id' => $idMap[$description],
+                        'description' => $description,
+                        'hs' => $hs,
+                    ]);
+                }
+                if($addData) {
+                    $data = [];
+                    foreach ($addData as $description => $hs) {
+                        $data[] = [
+                            'description' => $description,
+                            'hs' => $hs,
+                        ];
+                    }
+                    $model->addAll($data);
+                }
+            }
             $newFileName = time() . rand(1000, 9999) . '.' . $ext;
-            
             PHPExcel_IOFactory::createWriter($excel, $IOFactoryType)->save(AssetsController::$basePath . $newFileName);
             $this->ajaxSuccess($newFileName);
         } else {
@@ -57,16 +90,21 @@ class HsController extends AuthController {
         if($desIndex !== -1 && $hsIndex !== -1) {
             $sheet = $excel->setActiveSheetIndex($index);
             $colName = excelColumn($hsIndex);
+            $update = [];
             for($i = 1; $i < count($sheetArray); $i++) {
-                if(
-                    $sheetArray[$i][$desIndex]
-                    && ($hs = $map[descriptionFormat($sheetArray[$i][$desIndex])])
-                    && (
-                        !$sheetArray[$i][$hsIndex] || !$emptyOnly
-                )) {
+                $description = descriptionFormat($sheetArray[$i][$desIndex]);
+                $hs = $map[$description];
+                $excelHs = $sheetArray[$i][$hsIndex];
+                if($description) {
+                    if($hs && (!$excelHs || !$emptyOnly)) {
                         $sheet->setCellValue($colName . ($i + 1), $hs);
+                    }
+                    if($excelHs && $emptyOnly) {
+                        $update[$description] = $excelHs;
+                    }
                 }
             }
+            return $update;
         }
     }
 
